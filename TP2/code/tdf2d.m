a35c4c28fcd0cdc4edde13afc056146db482ea35
Gonzalo Castiglione <gcastigl@gmail.com>
2013-06-20 16:32:15 -0300

    aptimizo un poco la cantidad de mutiplicaciones para tdf2d

--- TP2/code/tdf2d.m
+++ TP2/code/tdf2d.m
@@ -1,12 +1,13 @@
 function transform = tdf2d(matrix)
 	N = rows(matrix);
 	transform = [];
+	aux = i * 2 * pi / N;
 	for l = 1 : N
 		for k = 1 : N
 			transform(l, k) = 0;
 			for n = 1 : N
 				for m = 1 : N
-					c = e ** (i * 2 * pi * (n * l + m * k) / N);
+					c = e ** (aux * (n * l + m * k));
 					transform(l, k) += matrix(n, m) * c;
 			    endfor
 		    endfor
