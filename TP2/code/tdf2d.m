47ea05c8b9ed2b89bc91d1e5185138d26e5357f0
axel wassington <axelwass@gmail.com>
2013-06-20 16:51:57 -0300

    resta 1

--- TP2/code/tdf2d.m
+++ TP2/code/tdf2d.m
@@ -1,16 +1,17 @@
 function transform = tdf2d(matrix)
 	N = rows(matrix);
-	transform = [];
-	aux = i * 2 * pi / N;
+	transform = zeros(N,N);
+	aux = -i * 2 * pi / N;
 	for l = 1 : N
 		for k = 1 : N
-			transform(l, k) = 0;
 			for n = 1 : N
 				for m = 1 : N
-					c = e ** (aux * (n * l + m * k));
+					c = e ** (aux * ((n-1) * (l-1) + (m-1) * (k-1)));
 					transform(l, k) += matrix(n, m) * c;
 			    endfor
 		    endfor
 		endfor
 	endfor
 endfunction
+
+
