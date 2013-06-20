a14728b5e80048d478385230701b592f606f76cf
Gonzalo Castiglione <gcastigl@gmail.com>
2013-06-20 15:42:30 -0300

    subo ejercicio 1 finalizado. Borro tex ya que no se va a usar.

--- TP2/code/tdf2d.m
+++ TP2/code/tdf2d.m
@@ -1,10 +1,14 @@
-function X = tdf2d(matrix, N, l, k)
-	X = [];
-   #N = length(matrix(1,:);
-    for n = 1 : N
-		for m = 1 : N
-			c = e ** (i * 2 * pi / N );
-    		X(n, m) = matrix(n, m);
-	    endfor
-    endfor
+function transform = tdf2d(matrix)
+	N = rows(matrix);
+	transform = [];
+	for l = 1 : N
+		for k = 1 : N
+			for n = 1 : N
+				for m = 1 : N
+					c = e ** (i * 2 * pi * (n * l + m * k) / N);
+    				transform(n, m) = matrix(n, m) * c;
+			    endfor
+		    endfor
+		endfor
+	endfor
 endfunction
