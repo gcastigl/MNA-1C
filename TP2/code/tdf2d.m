a77366d4b563d5fac646641c4180d5f73b9882ee
Gonzalo Castiglione <gcastigl@gmail.com>
2013-06-20 14:43:32 -0300

    subo enunciado del TP2. datos para el tp2. primera parte del informe.

---
+++ TP2/code/tdf2d.m
@@ -0,0 +1,10 @@
+function X = tdf2d(matrix, N, l, k)
+	X = [];
+   #N = length(matrix(1,:);
+    for n = 1 : N
+		for m = 1 : N
+			c = e ** (i * 2 * pi / N );
+    		X(n, m) = matrix(n, m);
+	    endfor
+    endfor
+endfunction
