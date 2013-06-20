f4093b6991eb23e6e1303099bea3acbbe4c6f0c4
Gonzalo Castiglione <gcastigl@gmail.com>
2013-06-20 16:26:27 -0300

    corrijo error al calcular la tdf2d y agrego imagenes la informe

--- TP2/code/tdf2d.m
+++ TP2/code/tdf2d.m
@@ -3,10 +3,11 @@ function transform = tdf2d(matrix)
 	transform = [];
 	for l = 1 : N
 		for k = 1 : N
+			transform(l, k) = 0;
 			for n = 1 : N
 				for m = 1 : N
 					c = e ** (i * 2 * pi * (n * l + m * k) / N);
-    				transform(n, m) = matrix(n, m) * c;
+					transform(l, k) += matrix(n, m) * c;
 			    endfor
 		    endfor
 		endfor
