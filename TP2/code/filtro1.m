d835010b45579e7fd338d9ceb06cd10f5b06405a
Axel <axelwass@gmail.com>
2013-06-20 22:49:26 -0300

    saltos de linea

--- TP2/code/filtro1.m
+++ TP2/code/filtro1.m
@@ -2,7 +2,7 @@ function H = filtro1(N)
 	H = zeros(N,N);
 	for k = 1:N
 		for l = 1:N
-			if(!((l>190 && l<210) || (k>190 && k<210)))
+			if(((l>190 && l<210) || (k>190 && k<210)))
 				H(k,l) = 1;
 			endif
 		endfor
