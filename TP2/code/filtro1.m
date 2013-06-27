c831965014cd74d44e3506abeb35aa5729b6b949
Axel <axelwass@gmail.com>
2013-06-26 23:20:25 -0300

    cambios en los filtros

--- TP2/code/filtro1.m
+++ TP2/code/filtro1.m
@@ -2,8 +2,9 @@ function H = filtro1(N)
 	H = zeros(N,N);
 	for k = 1:N
 		for l = 1:N
+			H(k,l) = 1;
 			if(((l>190 && l<210) || (k>190 && k<210)))
-				H(k,l) = 1;
+				H(k,l) = 0;
 			endif
 		endfor
 	endfor
--- TP2/code/filtroGauss.m
+++ TP2/code/filtroGauss.m
@@ -1,8 +1,8 @@
-function H = filtroGauss(N)
+function H = filtroGauss(N, potencia)
 	H = zeros(N,N);
 	for k = 1:N
 		for l = 1:N
-			H(k,l) = exp(-0.1 * (k**2 + l**2));
+			H(k,l) = exp(-potencia*(k**2 + l**2));
 		endfor
 	endfor
 
